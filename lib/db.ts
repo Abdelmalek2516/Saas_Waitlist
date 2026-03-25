5c99eca947ccee8f73553af7eeb87acc3c997b3b
Abdelmalek Aityounes <abdelmalekaityounes@gmail.com>
2026-03-25 19:30:40 +0100

    first commit:setup mongoDB connection and user schema

---
+++ lib/db.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local",
+  );
+}
+
+// We use a global variable to cache the connection across hot reloads in development.
+// This prevents exhausting database connections.
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false, // Disables Mongoose buffering, forcing errors if not connected
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      console.log("✅ Successfully connected to MongoDB");
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
